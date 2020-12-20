3a9badc59c55b530def94259c14dd3ecff4d1339
Yann Depps <yann.deperrot@protonmail.com>
2020-12-20 18:42:08 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Coding Maths
+
+-   [Introduction](src/ep_01/app.js)
+-   [Intro To Trigonometry](src/ep_02/app.js)
---
+++ src/ep_01/app.js
@@ -0,0 +1,16 @@
+window.onload = function () {
+  const canvas = document.getElementById("canvas"),
+    context = canvas.getContext("2d"),
+    width = (canvas.width = window.innerWidth),
+    height = (canvas.height = window.innerHeight);
+
+  context.fillRect(0, 0, width, height);
+  context.strokeStyle = "#f2f2f2";
+
+  for (let i = 0; i < 100; i++) {
+    context.beginPath();
+    context.moveTo(Math.random() * width, Math.random() * height);
+    context.lineTo(Math.random() * width, Math.random() * height);
+    context.stroke();
+  }
+};
